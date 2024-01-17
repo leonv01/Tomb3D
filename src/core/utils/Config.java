f4870bbc895b19bde32ed28fa8a3fdba9f29f0d7
Leon <leonvogt2001@gmail.com>
2024-01-17 21:26:04 +0100

    working death screen

--- src/core/utils/Config.java
+++ src/core/utils/Config.java
@@ -40,8 +40,8 @@ public class Config {
     public final static int HEAVY_ENEMY_SCORE = 300;
     public final static double HEAVY_ENEMY_SPEED = 0.01;
 
-    public final static int BOSS_ENEMY_DAMAGE = 16;
-    public final static int BOSS_ENEMY_HEALTH = 400;
+    public final static int BOSS_ENEMY_DAMAGE = 17;
+    public final static int BOSS_ENEMY_HEALTH = 600;
     public final static int BOSS_ENEMY_SCORE = 400;
     public final static double BOSS_ENEMY_SPEED = 0.005;
 
